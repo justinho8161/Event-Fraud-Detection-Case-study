3a3cbc4a6a8c28cda8f961dcb915dc37a8595ad0
Justin Ho <44845829+justinho8161@users.noreply.github.com>
2020-12-12 11:37:52 -0500

    migrating_files

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, redirect, render_template
+from pymongo import MongoClient
+from flask_bootstrap import Bootstrap
+import pickle
+from justin import update_db, pull_values
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template("index.html")
+
+
+@app.route('/score', methods=['POST'])
+def score():
+   try:
+      update_db(model)
+   except:
+      pass
+
+   low, medium, high = pull_values()
+
+   return render_template("score.html", low=low, medium=medium, high=high)
+
+if __name__ == '__main__':
+   mongo_client=MongoClient()
+   db = mongo_client['Fraud_Detection']
+   collection = db['Events']
+   with open('model_525.pkl', 'rb') as f:
+      model = pickle.load(f)
+
+   app.run(host='0.0.0.0', port=8080, debug=True)
+
+
+    
+       
+
---
+++ class_features_final_525.py
@@ -0,0 +1,130 @@
+import pandas as pd
+import numpy as np
+from itertools import *
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+
+
+class Features:
+
+    def __init__(self):
+        self.X = pd.DataFrame(data=None, columns=['body_length'])
+
+    def features_clean(self, df):
+        self.X['object_id'] = df['object_id']
+        self.X['body_length'] = df['body_length']
+        self.X['name_length'] = df['name_length']
+        self.X['sale_duration'] = df['sale_duration'].fillna(df['sale_duration'].mean())
+        self.X['user_age'] = df['user_age']
+        self.X['fb_published'] = df['fb_published']
+        self.X['has_logo'] = df['has_logo']
+        self.X['has_analytics'] = df['has_analytics']
+        self.X['org_facebook'] = df['org_facebook'].fillna(df['org_facebook'].mean())
+        self.X['org_twitter'] = df['org_twitter'].fillna(df['org_twitter'].mean())
+       
+        selected_dummies = ['country','venue_state','currency','delivery_method',
+                            'payout_type']
+        self.X = self.dummies(df.loc[:,selected_dummies])
+        self.X = self.ticket_types(df.loc[:,'ticket_types'])
+        self.X=self.X.drop('object_id',axis=1)
+        
+        return self.X
+
+    def ticket_types(self,tickets):
+    
+        event_types = pd.DataFrame(list(chain.from_iterable(tickets)))
+        #event_types = event_types.drop('quantity_sold',axis=1)
+        totals = event_types.groupby('event_id').sum()
+        avg_cost = event_types.groupby(['event_id','availability','quantity_total'
+                                        ]).sum().groupby('event_id').mean()
+        unique_event_ids = pd.merge(totals, avg_cost, on=['event_id']).reset_index()
+        unique_event_ids = unique_event_ids.drop(columns=['cost_x', 'availability']).rename(
+                index=str, columns={"event_id":"object_id",
+                                    "cost_y": "avg_ticket_cost", 
+                                    "quantity_total": "tot_ticket_quant"
+                                    })
+        self.X=pd.merge(self.X, unique_event_ids, on='object_id',how='outer')
+        
+        #Dummy if ticket types are missing
+#        self.X['ticket_type_missing'] = pd.get_dummies(
+#                self.X['avg_ticket_cost'].map({np.nan:1}))
+        
+        self.X['ticket_type_missing'] = self.X['avg_ticket_cost'].where(self.X['avg_ticket_cost'].isnull(),0)
+        self.X['ticket_type_missing'] = self.X['ticket_type_missing'].where(self.X['ticket_type_missing']==0,1)
+        
+        self.X['avg_ticket_cost'] = self.X['avg_ticket_cost'].fillna(self.X['avg_ticket_cost'].mean())
+        self.X['tot_ticket_quant'] = self.X['tot_ticket_quant'].fillna(self.X['tot_ticket_quant'].mean())
+        #self.X['tot_tickets_sold'] = self.X['tot_tickets_sold'].fillna(self.X['tot_tickets_sold'].mean())
+
+        return self.X
+
+
+    def dummies(self, df):
+        
+##        #Country
+#        dummies_country = pd.get_dummies(df['country'])
+##        self.X = pd.concat([dummies_country, self.X], 
+##                           axis=1, join_axes=[self.X.index])
+        
+#        #State
+#        dummies_state = pd.get_dummies(df['venue_state'])
+#        self.X = pd.concat([dummies_state, self.X], 
+#                           axis=1, join_axes=[self.X.index])
+
+        #Currency
+#        dummies_currency = pd.get_dummies(df['currency'])
+#        self.X = pd.concat([dummies_currency, self.X], 
+#                           axis=1, join_axes=[self.X.index])
+    
+        self.X['AUD']=df['currency'].where(df['currency']=='AUD',0)
+        self.X['AUD']=self.X['AUD'].where(self.X['AUD']==0,1)
+        
+        self.X['CAD']=df['currency'].where(df['currency']=='CAD',0)
+        self.X['CAD']=self.X['CAD'].where(self.X['CAD']==0,1)
+        
+        self.X['EUR']=df['currency'].where(df['currency']=='EUR',0)
+        self.X['EUR']=self.X['EUR'].where(self.X['EUR']==0,1)
+        
+        self.X['GBP']=df['currency'].where(df['currency']=='GBP',0)
+        self.X['GBP']=self.X['GBP'].where(self.X['GBP']==0,1)
+        
+        self.X['MXN']=df['currency'].where(df['currency']=='MXN',0)
+        self.X['MXN']=self.X['MXN'].where(self.X['MXN']==0,1)
+        
+        self.X['NZD']=df['currency'].where(df['currency']=='NZD',0)
+        self.X['NZD']=self.X['NZD'].where(self.X['NZD']==0,1)
+        
+        self.X['USD']=df['currency'].where(df['currency']=='USD',0)
+        self.X['USD']=self.X['USD'].where(self.X['USD']==0,1)
+        
+    
+        #Delivery method
+#        dummies_delivery_method = pd.get_dummies(df['delivery_method'])
+#        self.X = pd.concat([dummies_delivery_method, self.X], 
+#                           axis=1, join_axes=[self.X.index])
+        
+        self.X['deliv_method_0']=df['delivery_method'].where(df['delivery_method']==0.0,0)
+        self.X['deliv_method_0']=self.X['deliv_method_0'].where(self.X['deliv_method_0']==0,1)
+        
+        self.X['deliv_method_1']=df['delivery_method'].where(df['delivery_method']==1.0,0)
+        self.X['deliv_method_1']=self.X['deliv_method_1'].where(self.X['deliv_method_1']==0,1)
+        
+        self.X['deliv_method_3']=df['delivery_method'].where(df['delivery_method']==3.0,0)
+        self.X['deliv_method_3']=self.X['deliv_method_3'].where(self.X['deliv_method_3']==0,1)
+        
+        #Payout type
+#        dummies_payout_type = pd.get_dummies(df['payout_type'])
+#        self.X = pd.concat([dummies_payout_type, self.X], 
+#                           axis=1, join_axes=[self.X.index])
+#        
+        self.X['payout_type_cash']=df['payout_type'].where(df['payout_type']=='ACH',0)
+        self.X['payout_type_cash']=self.X['payout_type_cash'].where(self.X['payout_type_cash']==0,1)
+        
+        self.X['payout_type_check']=df['payout_type'].where(df['payout_type']=='CHECK',0)
+        self.X['payout_type_check']=self.X['payout_type_check'].where(self.X['payout_type_check']==0,1)
+        
+        self.X['payout_type_missing']=df['payout_type'].where(df['payout_type']==np.nan,0)
+        self.X['payout_type_missing']=self.X['payout_type_missing'].where(self.X['payout_type_missing']==0,1)
+    
+    
+        return self.X
